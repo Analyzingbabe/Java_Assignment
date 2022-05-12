8eeb497e21dde2d03cccf69f003056f8b6998086
balaarumugam <balaarumugam@meena.attlocal.net>
2022-05-11 23:19:40 -0400

    assignment 3&4

--- Java_assignment/src/main/java/Findind_sum_of_odd_and_even_numbers.java
+++ Java_assignment/src/main/java/Findind_sum_of_odd_and_even_numbers.java
@@ -1,9 +1,52 @@
 
+import java.util.Arrays;
+import java.util.Scanner;
 public class Findind_sum_of_odd_and_even_numbers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
-	}
+		
 
-}
+		
+				int sum=0;
+				int sum1=0;
+				
+				 Scanner number=new Scanner(System.in);
+		  	  System.out.println("Enter the no.of elements in the array :");
+		  	  int n=number.nextInt();
+		  	  int []arr1=new int[n];
+		  //	System.out.println("Enter the no.of elements in the array :");
+		  	  System.out.println("Enter the elements of the array:");
+		  	  
+		  	  for(int i=0;i<n;i++) {
+		  		  arr1[i]=number .nextInt();
+		  		if (arr1[i]%2==0) {
+		  			sum=sum+arr1[i];
+		  			
+		  		}
+		  		else if (arr1[i]%2!=0) {
+		  			sum1=sum1+arr1[i];
+		  			
+		  		
+		  		}
+		  	  
+		  	  }
+		  	  System.out.println("The Array elements are"+Arrays.toString(arr1));
+			
+		      //for(int g: arr1) {
+		    	  
+		    	//  System.out.println(g);
+		    //  }
+			
+			
+		      
+				
+			System.out.println("Sum of even numbers is :=" + sum);
+			
+			System.out.println("Sum of odd number is:="+sum1);
+			}
+
+		}
+	
